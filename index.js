cc0225e805c71ef621609eea1b2d60825876d05f
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-08-18 03:46:40 -0700

    Set window title and background color

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let window;
 electron.app.on("ready", () => {
 
     //Create window
-    window = new electron.BrowserWindow({ webPreferences: { nodeIntegration: true } });
+    window = new electron.BrowserWindow({ title: "MongoGlass", backgroundColor: "#000000", webPreferences: { nodeIntegration: true } });
     window.maximize();
 
     //Remove window reference when closed
